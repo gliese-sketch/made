f81ae9b511641b1d062d88336054080e0d06b367
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-12 11:20:04 +0000

    show typing animation

--- client/components/Messages/Typing.jsx
+++ client/components/Messages/Typing.jsx
@@ -1,7 +1,14 @@
 export default function Typing({ users }) {
   return users.length > 0 ? (
-    <div>
-      {users.join(", ")} {users.length === 1 ? "is" : "are"} typing...
+    <div className="flex items-center gap-1 text-gray-600">
+      <div className="typing">
+        <span className="circle scaling"></span>
+        <span className="circle scaling"></span>
+        <span className="circle scaling"></span>
+      </div>
+      <div>
+        {users.join(", ")} {users.length === 1 ? "is" : "are"} typing...
+      </div>
     </div>
   ) : (
     <></>
